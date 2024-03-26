a60883574094ee856614003fd1ea524e07210298
unknown <minleliang2002@gmail.com>
2024-03-25 22:39:18 -0400

    integration tests

--- videogame/tests/integration.test.js
+++ videogame/tests/integration.test.js
@@ -1,7 +1,7 @@
   const request = require('supertest');
-  const {app, server} = require('./app');
+  const {app, server} = require('../app');
   const mongoose = require('mongoose');
-  const VideoGame = require('./models/games');
+  const VideoGame = require('../../models/games');
 
   describe('VIDEOGAME', () => {
     before(async () => {
